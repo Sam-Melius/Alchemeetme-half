e7edd1f59e09ab584625aad3ed21acc1e6b5edb5
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-04-25 15:59:02 -0700

    wrote test for getting header. test passing...i think.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,17 @@
+import { render ,screen, waitfor} from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from './App';
 
-test('Should render the header', () => {
 
+describe('App', () => {
+    test('Should render the header', () => {
+        render(
+            <MemoryRouter>
+                <App />
+            </MemoryRouter>
+        );
+        screen.findByAltText('header');
+        
+    })
 })
+
