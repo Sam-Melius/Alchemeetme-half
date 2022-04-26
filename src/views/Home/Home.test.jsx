b6f9b739908dc5e19b2ee0b6276d97243d7fdca4
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-04-25 17:10:16 -0700

    wrote test for getting profile name. Test is passing

--- src/views/Home/Home.test.jsx
+++ src/views/Home/Home.test.jsx
@@ -1,15 +1,32 @@
+import { render, screen, waitfor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Home from './Home';
 
-const user = {
-  id: 1,
-  created_at: '2021-12-13T00:17:29+00:00',
-  name: 'Vonta',
-  avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
-  header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
-  likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
-  motto: 'Res Non Verba',
-  color: 'crimson',
-}
 
-test('Should render the user profile', () => {
+  const user = {
+    id: 1,
+    created_at: '2021-12-13T00:17:29+00:00',
+    name: 'Vonta',
+    avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
+    header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
+    likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
+    motto: 'Res Non Verba',
+    color: 'crimson',
+  }
+  
+  test('Should render the user profile', async () => {
+    render(
+      
+        <Home user={user} />
+      
+    );
+    const { name } = user;
+    
+    
+    const profName = screen.getByRole('heading', { name });
 
-})
+    expect(profName).toBeInTheDocument();
+
+
+  })
+
